957df3b0e8a3755017cf2a53396660b88bc58449
Anmol Srivastava <anmolsrirocks@gmail.com>
2020-12-03 18:53:45 +0530

    Changes Made

--- Template.cpp
+++ Template.cpp
@@ -14,10 +14,10 @@
 #define MOD2 998244353
 using namespace std;
 
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
-template <typename T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// #include <ext/pb_ds/tree_policy.hpp>
+// #include <ext/pb_ds/assoc_container.hpp>
+// using namespace __gnu_pbds;
+// template <typename T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 // order_of_key (k) : Number of items strictly smaller than k 
 // find_by_order(k) : K-th element in a set (counting from zero)
 
@@ -29,7 +29,8 @@ template<class H, class... T> void dbvar(H h, T... t){
 	dbvar(t...);
 }
 
-const ll INF = (ll)1e18;
+const ll INFL = (ll)1e18;
+const int INF = (int)1e9;
 const ld eps = (ld)1e-9;
 const ld pi = acos(-1.0);
 const int dx[4] = {-1, 0, 1, 0};
